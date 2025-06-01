20dfe9681b822e7c51e28eea3d8fda56c0692476
Jan Hiroshi Steinkamp <janhiroshisteinkamp@Jans-MacBook-Air.local>
2025-06-01 17:38:58 +0200

    MODE

--- Parsing.cpp
+++ Parsing.cpp
@@ -75,11 +75,18 @@ void commandParsing(char *messagebuffer, std::vector<pollfd> &fds, size_t i)
         }
     }
 
+bool isSpaceOrNewline(char c) {
+        return std::isspace(static_cast<unsigned char>(c)) || c == '\r' || c == '\n';
+    }
+
 void handleMode(User* requester,
                 const std::string& chanName,
                 const std::string& flags,
                 const std::vector<std::string>& tokens, std::vector<pollfd> &fds)
 {
+    std::cerr << "[DEBUG] flags received: '" << flags << "'\n";
+for (size_t i = 0; i < flags.size(); ++i)
+    std::cerr << "[DEBUG] char[" << i << "]: '" << flags[i] << "' (" << int(flags[i]) << ")\n";
     std::map<std::string,Chatroom*>::iterator itChan
         = g_chatrooms.find(chanName);
     if (itChan == g_chatrooms.end()) {
@@ -109,9 +116,11 @@ void handleMode(User* requester,
     bool adding = true;
     size_t argIdx = 3;                  
     std::vector<std::string> argList;   
-
-    for (std::string::const_iterator fit = flags.begin();
-         fit != flags.end();
+    std::string cleanFlags = flags;
+cleanFlags.erase(std::remove_if(cleanFlags.begin(), cleanFlags.end(), isSpaceOrNewline),
+                 cleanFlags.end());  
+        for (std::string::const_iterator fit = cleanFlags.begin();
+         fit != cleanFlags.end();
          ++fit)
     {
         char m = *fit;
